edd9ca8f2ee583dfc99f20d9d655017be9ba0c21
Triet Lieu <trietmlieu@gmail.com>
2021-05-26 16:54:15 -0700

    Updates

---
+++ precache-manifest.164c0cdc4c615e463733e0ba91209b87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1054b450a25334298707002096f35e19",
+    "url": "/react-ecomm-sample/index.html"
+  },
+  {
+    "revision": "0142fa6637efc6244059",
+    "url": "/react-ecomm-sample/static/css/main.c35e003f.chunk.css"
+  },
+  {
+    "revision": "7da2cf574c4bcdb44bcd",
+    "url": "/react-ecomm-sample/static/js/2.752c62b0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-ecomm-sample/static/js/2.752c62b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0142fa6637efc6244059",
+    "url": "/react-ecomm-sample/static/js/main.535316d9.chunk.js"
+  },
+  {
+    "revision": "997a53edebca417197e6",
+    "url": "/react-ecomm-sample/static/js/runtime-main.e253932b.js"
+  },
+  {
+    "revision": "e4072dc0ea58c0cfbe4f4a0ad941b501",
+    "url": "/react-ecomm-sample/static/media/banner.e4072dc0.jpg"
+  }
+]);
